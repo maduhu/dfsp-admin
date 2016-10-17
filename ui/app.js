6ae0981f0a5edeee8cda6b2dd21e87b6da45a446
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-10-17 13:14:59 +0300

    fix: refactor front-end boilerplate

--- ui/app.js
+++ ui/app.js
@@ -6,6 +6,7 @@ import {
 import implReducers from './reducers'
 import UtRuleReducers from 'ut-rule/ui/react/reducers'
 import UtRuleRoutes from 'ut-rule/ui/react/routes'
+import { Route, Redirect } from 'react-router';
 
 module.exports = {
   init: function (bus) {
@@ -16,12 +17,20 @@ module.exports = {
     document.title = 'DFSP Admin'
   },
   load: function () {
+    const routes = (
+      <Route>
+        <Redirect from='/' to='rule' />
+        <Route path='rule'>
+          {UtRuleRoutes}
+        </Route>
+      </Route>
+    );
     ReactDOM.render(
       <UtFront
         reducers={{...implReducers, ...UtRuleReducers}}
         utBus={this.bus}
         environment={!this.bus.config || !this.bus.config.debug ? 'production' : ''}>
-          {UtRuleRoutes}
+          {routes}
       </UtFront>,
       document.getElementById('utApp')
     )
