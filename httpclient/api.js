c2e4ff35c63642db26d4e7e52b001f8b2004ad9f
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-08 11:49:08 +0200

    fix: change mock data according to the changes applied in ut-rule

--- httpclient/api.js
+++ httpclient/api.js
@@ -6,103 +6,91 @@ module.exports = {
   method: 'post',
   'rule.item.fetch': function (msg, $meta) {
     $meta.mtid = 'response'
-    var result = []
-    var params = msg.payload.params.length ? msg.payload.params : null
-    var check = function (key) {
-      return !params || ~params.indexOf(key)
-    }
-    if (check('currency')) {
-      result = result.concat([{
-        type: 'currency',
-        value: 'USD',
-        display: 'USD'
-      },{
-        type: 'currency',
-        value: 'TZS',
-        display: 'TZS'
-      }])
-    }
-    if (check('channel')) {
-      result = result.concat([{
-        type: 'channel',
-        value: 1,
-        display: 'USSD'
-      }])
-    }
-    if (check('country')) {
-      result = result.concat([{
-        type: 'country',
-        value: 1,
-        display: 'USA'
-      }])
-    }
-    if (check('region')) {
-      result = result.concat([{
-        type: 'region',
-        value: 1,
-        display: 'West'
-      }])
-    }
-    if (check('city')) {
-      result = result.concat([{
-        type: 'city',
-        value: 1,
-        display: 'Seattle'
-      }, {
-        type: 'city',
-        value: 2,
-        display: 'Redmond'
-      }, {
-        type: 'city',
-        value: 3,
-        display: 'San Francisco'
-      }])
-    }
-    if (check('organization')) {
-      result = result.concat([{
-        type: 'organization',
-        value: 1,
-        display: 'Organization Name'
-      }])
-    }
-    if (check('role')) {
-      result = result.concat([{
-        type: 'role',
-        value: 1,
-        display: 'Agent'
-      }])
-    }
-    if (check('operation')) {
-      result = result.concat([{
-        type: 'operation',
-        value: 1,
-        display: 'P2P Transfer'
-      }])
-    }
-    if (check('supervisor')) {
-      result = result.concat([{
-        type: 'supervisor',
-        value: 1,
-        display: 'CEO'
-      }])
-    }
-    if (check('product')) {
-      result = result.concat([{
-        type: 'product',
-        value: 1,
-        display: 'Product name'
-      }])
-    }
-    if (check('account')) {
-      result = result.concat([{
-        type: 'account',
-        value: 1,
-        display: 'Account name'
-      }])
-    }
     return {
       payload: {
-        result: result
+        result: [
+          // currency
+          {
+            type: 'currency',
+            value: 'USD',
+            display: 'USD'
+          },
+          {
+            type: 'currency',
+            value: 'TZS',
+            display: 'TZS'
+          },
+          // channel
+          {
+            type: 'channel',
+            value: 1,
+            display: 'USSD'
+          },
+          // country
+          {
+            type: 'country',
+            value: 1,
+            display: 'USA'
+          },
+          // region
+          {
+            type: 'region',
+            value: 1,
+            display: 'West'
+          },
+          // city
+          {
+            type: 'city',
+            value: 1,
+            display: 'Seattle'
+          },
+          {
+            type: 'city',
+            value: 2,
+            display: 'Redmond'
+          },
+          {
+            type: 'city',
+            value: 3,
+            display: 'San Francisco'
+          },
+          // organization
+          {
+            type: 'organization',
+            value: 1,
+            display: 'Organization Name'
+          },
+          // role
+          {
+            type: 'role',
+            value: 1,
+            display: 'Agent'
+          },
+          // operation
+          {
+            type: 'operation',
+            value: 1,
+            display: 'P2P Transfer'
+          },
+          // supervisor
+          {
+            type: 'supervisor',
+            value: 1,
+            display: 'CEO'
+          },
+          // product
+          {
+            type: 'product',
+            value: 1,
+            display: 'Product name'
+          },
+          // account
+          {
+            type: 'account',
+            value: 1,
+            display: 'Account name'
+          }
+        ]
       }
     }
   }
