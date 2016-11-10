952c4b143763ea65879f7b79adbdb1c0950887de
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-10 16:37:52 +0200

    fix: reformat mock nomenclatures backend response

--- httpclient/api.js
+++ httpclient/api.js
@@ -8,89 +8,91 @@ module.exports = {
     $meta.mtid = 'response'
     return {
       payload: {
-        result: [
-          // currency
-          {
-            type: 'currency',
-            value: 'USD',
-            display: 'USD'
-          },
-          {
-            type: 'currency',
-            value: 'TZS',
-            display: 'TZS'
-          },
-          // channel
-          {
-            type: 'channel',
-            value: 1,
-            display: 'USSD'
-          },
-          // country
-          {
-            type: 'country',
-            value: 1,
-            display: 'USA'
-          },
-          // region
-          {
-            type: 'region',
-            value: 1,
-            display: 'West'
-          },
-          // city
-          {
-            type: 'city',
-            value: 1,
-            display: 'Seattle'
-          },
-          {
-            type: 'city',
-            value: 2,
-            display: 'Redmond'
-          },
-          {
-            type: 'city',
-            value: 3,
-            display: 'San Francisco'
-          },
-          // organization
-          {
-            type: 'organization',
-            value: 1,
-            display: 'Organization Name'
-          },
-          // role
-          {
-            type: 'role',
-            value: 1,
-            display: 'Agent'
-          },
-          // operation
-          {
-            type: 'operation',
-            value: 1,
-            display: 'P2P Transfer'
-          },
-          // supervisor
-          {
-            type: 'supervisor',
-            value: 1,
-            display: 'CEO'
-          },
-          // product
-          {
-            type: 'product',
-            value: 1,
-            display: 'Product name'
-          },
-          // account
-          {
-            type: 'account',
-            value: 1,
-            display: 'Account name'
-          }
-        ]
+        result: {
+          items: [
+            // currency
+            {
+              type: 'currency',
+              value: 'USD',
+              display: 'USD'
+            },
+            {
+              type: 'currency',
+              value: 'TZS',
+              display: 'TZS'
+            },
+            // channel
+            {
+              type: 'channel',
+              value: 1,
+              display: 'USSD'
+            },
+            // country
+            {
+              type: 'country',
+              value: 1,
+              display: 'USA'
+            },
+            // region
+            {
+              type: 'region',
+              value: 1,
+              display: 'West'
+            },
+            // city
+            {
+              type: 'city',
+              value: 1,
+              display: 'Seattle'
+            },
+            {
+              type: 'city',
+              value: 2,
+              display: 'Redmond'
+            },
+            {
+              type: 'city',
+              value: 3,
+              display: 'San Francisco'
+            },
+            // organization
+            {
+              type: 'organization',
+              value: 1,
+              display: 'Organization Name'
+            },
+            // role
+            {
+              type: 'role',
+              value: 1,
+              display: 'Agent'
+            },
+            // operation
+            {
+              type: 'operation',
+              value: 1,
+              display: 'P2P Transfer'
+            },
+            // supervisor
+            {
+              type: 'supervisor',
+              value: 1,
+              display: 'CEO'
+            },
+            // product
+            {
+              type: 'product',
+              value: 1,
+              display: 'Product name'
+            },
+            // account
+            {
+              type: 'account',
+              value: 1,
+              display: 'Account name'
+            }
+          ]
+        }
       }
     }
   }
