e0c4b89f2142831c9ee2d3d9359e371ea5421d48
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-02-17 19:34:36 +0200

    fix: fix webpack

--- webpack.js
+++ webpack.js
@@ -8,7 +8,15 @@ var wb = require('ut-front/webpack/ut-front.config')({
   outputPath: path.resolve(__dirname, 'dist'),
   cssImport: {
     path: path.resolve(__dirname, 'config')
-  }
+  },
+  cssAssets: {loadPaths: [
+    path.resolve(__dirname, 'ui', 'administration', 'assets', 'images'),
+    path.resolve(path.join(path.dirname(require.resolve('ut-front-react')), 'components', '**', 'images')),
+    path.resolve(path.join(path.dirname(require.resolve('ut-front-react')), 'pages', '**', 'images')),
+    path.resolve(path.join(path.dirname(require.resolve('ut-front-react')), 'containers', '**', 'images')),
+    path.resolve(__dirname, 'ui', 'ut-bulk', 'ui', 'react', 'browser'),
+    path.resolve(__dirname, 'ui', 'browser', 'images')
+  ]}
 }, false)
 webpack(wb, (err, stats) => {
   if (err) {
