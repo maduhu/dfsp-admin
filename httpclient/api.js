c40387adcd62ccf9a8a8d997b6d151c1a4ce6164
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-04-04 10:30:15 +0300

    fix: fetch operation and role from backend

--- httpclient/api.js
+++ httpclient/api.js
@@ -19,131 +19,107 @@ module.exports = {
     }
   },
   'rule.item.fetch': function (msg, $meta) {
-    $meta.mtid = 'response'
-    return {
-      payload: {
-        id: 1,
-        result: {
-          items: [
-            // currency
-            {
-              type: 'currency',
-              value: 'USD',
-              display: 'USD'
-            },
-            {
-              type: 'currency',
-              value: 'TZS',
-              display: 'TZS'
-            },
-            // channel
-            {
-              type: 'channel',
-              value: 1,
-              display: 'USSD'
-            },
-            // country
-            {
-              type: 'country',
-              value: 1,
-              display: 'USA'
-            },
-            // region
-            {
-              type: 'region',
-              value: 1,
-              display: 'West'
-            },
-            // city
-            {
-              type: 'city',
-              value: 1,
-              display: 'Seattle'
-            },
-            {
-              type: 'city',
-              value: 2,
-              display: 'Redmond'
-            },
-            {
-              type: 'city',
-              value: 3,
-              display: 'San Francisco'
-            },
-            // organization
-            {
-              type: 'organization',
-              value: 1,
-              display: 'Organization Name'
-            },
-            // role
-            {
-              type: 'role',
-              value: 1,
-              display: 'Agent'
-            },
-            {
-              type: 'role',
-              value: 2,
-              display: 'Merchant'
-            },
-            {
-              type: 'role',
-              value: 3,
-              display: 'Customer'
-            },
-            {
-              type: 'role',
-              value: 1,
-              display: 'Agent'
-            },
-            // operation
-            {
-              type: 'operation',
-              value: 1,
-              display: 'P2P Transfer'
-            },
-            {
-              type: 'operation',
-              value: 2,
-              display: 'Invoce'
-            },
-            {
-              type: 'operation',
-              value: 4,
-              display: 'Bulk Payment'
-            },
-            {
-              type: 'operation',
-              value: 5,
-              display: 'Cash In'
-            },
-            {
-              type: 'operation',
-              value: 6,
-              display: 'Cash Out'
-            },
-            // supervisor
-            {
-              type: 'supervisor',
-              value: 1,
-              display: 'CEO'
-            },
-            // product
-            {
-              type: 'product',
-              value: 1,
-              display: 'Product name'
-            },
-            // account
-            {
-              type: 'account',
-              value: 1,
-              display: 'Account name'
-            }
-          ]
-        }
+    var data = [
+      // currency
+      {
+        type: 'currency',
+        value: 'USD',
+        display: 'USD'
+      },
+      {
+        type: 'currency',
+        value: 'TZS',
+        display: 'TZS'
+      },
+      // channel
+      {
+        type: 'channel',
+        value: 1,
+        display: 'USSD'
+      },
+      // country
+      {
+        type: 'country',
+        value: 1,
+        display: 'USA'
+      },
+      // region
+      {
+        type: 'region',
+        value: 1,
+        display: 'West'
+      },
+      // city
+      {
+        type: 'city',
+        value: 1,
+        display: 'Seattle'
+      },
+      {
+        type: 'city',
+        value: 2,
+        display: 'Redmond'
+      },
+      {
+        type: 'city',
+        value: 3,
+        display: 'San Francisco'
+      },
+      // organization
+      {
+        type: 'organization',
+        value: 1,
+        display: 'Organization Name'
+      },
+      // supervisor
+      {
+        type: 'supervisor',
+        value: 1,
+        display: 'CEO'
+      },
+      // product
+      {
+        type: 'product',
+        value: 1,
+        display: 'Product name'
+      },
+      // account
+      {
+        type: 'account',
+        value: 1,
+        display: 'Account name'
       }
-    }
+    ]
+
+    return this.bus.importMethod('identity.role.fetch')({})
+    .then((res) => {
+      data = data.concat(res.map((el) => {
+        return {
+          type: 'role',
+          value: el.roleId,
+          display: el.name
+        }
+      }))
+    })
+    .then(() => {
+      return this.bus.importMethod('ledger.transferType.fetch')({})
+      .then((res) => {
+        data = data.concat(res.map((el) => {
+          return {
+            type: 'operation',
+            value: el.transferTypeId,
+            display: el.name
+          }
+        }))
+        return {
+          payload: {
+            id: 1,
+            result: {
+              items:data
+            }
+          }
+        }
+      })
+    })
   }
 }
