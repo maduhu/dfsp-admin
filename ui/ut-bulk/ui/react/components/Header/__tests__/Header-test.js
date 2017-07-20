be2a0dcbc92e13db10f396658a62cce205959131
Kristiyan Serafimov <kristiyan.serafimov@softwaregroup-bg.com>
2017-07-20 11:34:09 +0300

    feat: replace karma with jest(much easier component unit testing)

---
+++ ui/ut-bulk/ui/react/components/Header/__tests__/Header-test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+
+import Header from '../index'
+import style from '../style.css'
+
+describe('A suite for <Header /> Component', function() {
+  let props = {
+    batchName: 'new',
+    batchStatus: 'new'
+  }
+
+  it('should contain 2 nested span elements', function() {
+    const wrapper = mount(<Header {...props} />)
+    expect(wrapper.find(`span`).length === 2).toBe(true)
+  })
+
+  it('should have a style.status class in the nested span(2)', function() {
+    const wrapper = mount(<Header {...props} />)
+    expect(wrapper.find('span').at(1).hasClass(style.status)).toBe(true)
+  })
+})
