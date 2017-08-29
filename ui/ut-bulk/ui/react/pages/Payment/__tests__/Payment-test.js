30aef7efd236d53f12ca5ce09cd9d4508f64880e
Ventsislav <ventsislav.dimitrov@softwaregroup.com>
2017-08-29 18:01:11 +0300

    fix: mount component instead of shallow render it

--- ui/ut-bulk/ui/react/pages/Payment/__tests__/Payment-test.js
+++ ui/ut-bulk/ui/react/pages/Payment/__tests__/Payment-test.js
@@ -1,6 +1,6 @@
 /* global it, describe, expect,beforeEach */
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import configureStore from 'redux-mock-store'
 import {Map, List} from 'immutable'
 import {Provider} from 'react-redux'
@@ -26,22 +26,102 @@ describe('A suite for <Payment /> Page', function () {
           actorId: '1'
         })
       })
+    }),
+    bulkPaymentToolbox: Map({
+      changeId: 0,
+      filters: Map({opened: true}),
+      buttons: Map({opened: false})
+    }),
+    bulkPaymentFilterStatus: Map({
+      statusId: [],
+      changeId: 0,
+      paymentStatus: List()
+    }),
+    bulkPaymentFilterDate: Map({
+      selectedDate: null,
+      changeId: 0
+    }),
+    bulkPaymentFilterCustom: Map({
+      field: 'name',
+      value: '',
+      changeId: 0
+    }),
+    bulkPaymentDetailEditPopup: Map({
+      item: Map({}),
+      changeId: 0
+    }),
+    bulkBatchPayPopup: Map({
+      batchId: null,
+      accounts: List(),
+      expirationDate: null,
+      startDate: null,
+      selectedAccount: null
+    }),
+    bulkBatchRejectPopup: Map({
+      batchId: null,
+      comment: null,
+      batchStatuses: List()
     })
   }
   const mockStore = configureStore()
   let store
+  let context = {
+    checkPermission: () => true,
+    dateFormat: () => '',
+    implementationStyle: {},
+    router: {
+      params: {
+        batchId: 1
+      }
+    },
+    muiTheme: {
+      prepareStyles: () => {},
+      baseTheme: {
+        spacing: '',
+        palette: {
+          textColor: ''
+        }
+      },
+      dropDownMenu: {
+        accentColor: ''
+      },
+      svgIcon: {
+        color: ''
+      },
+      textField: {
+        floatingLabelColor: ''
+      },
+      zIndex: {
+        dialog: ''
+      },
+      dialog: {
+        titleFontSize: ''
+      },
+      overlay: {
+        backgroundColor: ''
+      }
+    }
+  }
+  let childContextTypes = {
+    checkPermission: React.PropTypes.func,
+    dateFormat: React.PropTypes.func,
+    implementationStyle: React.PropTypes.array,
+    router: React.PropTypes.object,
+    muiTheme: React.PropTypes.object
+  }
+  let props = {
+    params: {
+      batchId: 1
+    }
+  }
 
   beforeEach(() => {
     store = mockStore(initialState)
   })
 
   it('should contain elements', function () {
-    const wrapper = shallow(<Provider store={store}><Payment /></Provider>)
+    const wrapper = mount(<Provider store={store}><Payment {...props} /></Provider>, {context, childContextTypes})
     expect(wrapper.length).toBe(1)
-    expect(wrapper.find('Page').at(0).length).toBe(1)
-    expect(wrapper.find('AddTab').at(0).length).toBe(1)
-    expect(wrapper.find('EditDetail').at(0).length).toBe(1)
-    expect(wrapper.find('PayBatch').at(0).length).toBe(1)
-    expect(wrapper.find('RejectBatch').at(0).length).toBe(1)
+    expect(wrapper.find('div').at(0).length).toBe(1)
   })
 })
