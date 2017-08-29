58bbb901ca1c078d9d879edccd8eb6f43561fece
Ventsislav <ventsislav.dimitrov@softwaregroup.com>
2017-08-29 16:46:32 +0300

    fix: mount component instead of shallow render it

--- ui/ut-bulk/ui/react/pages/Batch/__tests__/Batch-test.js
+++ ui/ut-bulk/ui/react/pages/Batch/__tests__/Batch-test.js
@@ -1,6 +1,6 @@
 /* global it, describe, expect,beforeEach */
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import configureStore from 'redux-mock-store'
 import {Map, List} from 'immutable'
 import {Provider} from 'react-redux'
@@ -20,6 +20,36 @@ describe('A suite for <Batch /> Page', function () {
       changeId: 0,
       fetchBatches: List()
     }),
+    bulkBatchToolbox: Map({
+      changeId: 0,
+      filters: Map({opened: true}),
+      buttons: Map({opened: false})
+    }),
+    bulkBatchFilterName: Map({
+      batchName: '',
+      changeId: 0
+    }),
+    bulkBatchFilterStatus: Map({
+      statusId: '__placeholder__',
+      changeId: 0,
+      batchStatuses: List()
+    }),
+    bulkBatchFilterDate: Map({
+      startDate: null,
+      endDate: null,
+      changeId: 0
+    }),
+    bulkPaymentGrid: Map({
+      data: List(),
+      checkedRows: Map(),
+      pagination: Map({
+        pageSize: 25,
+        pageNumber: 1,
+        recordsTotal: 0
+      }),
+      batch: Map(),
+      changeId: 0
+    }),
     login: Map({
       result: Map({
         'identity.check': Map({
@@ -29,19 +59,62 @@ describe('A suite for <Batch /> Page', function () {
     }),
     bulkBatchDetailEditPopup: Map({
       item: Map({})
+    }),
+    bulkBatchDeletePopup: Map({
+      batchId: null,
+      comment: null,
+      batchStatuses: List(),
+      changeId: 0
     })
   }
   const mockStore = configureStore()
   let store
+  let context = {
+    checkPermission: () => true,
+    dateFormat: () => '',
+    implementationStyle: {},
+    muiTheme: {
+      prepareStyles: () => {},
+      baseTheme: {
+        spacing: '',
+        palette: {
+          textColor: ''
+        }
+      },
+      dropDownMenu: {
+        accentColor: ''
+      },
+      svgIcon: {
+        color: ''
+      },
+      textField: {
+        floatingLabelColor: ''
+      },
+      zIndex: {
+        dialog: ''
+      },
+      dialog: {
+        titleFontSize: ''
+      },
+      overlay: {
+        backgroundColor: ''
+      }
+    }
+  }
+  let childContextTypes = {
+    checkPermission: React.PropTypes.func,
+    dateFormat: React.PropTypes.func,
+    implementationStyle: React.PropTypes.array,
+    muiTheme: React.PropTypes.object
+  }
 
   beforeEach(() => {
     store = mockStore(initialState)
   })
 
   it('should contain elements', function () {
-    const wrapper = shallow(<Provider store={store}><Batch /></Provider>)
+    const wrapper = mount(<Provider store={store}><Batch /></Provider>, {context, childContextTypes})
     expect(wrapper.length).toBe(1)
     expect(wrapper.find('div').at(0).length).toBe(1)
-    expect(wrapper.find('AddTab').at(0).length).toBe(1)
   })
 })
