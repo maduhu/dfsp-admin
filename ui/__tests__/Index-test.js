15c9d0c90892598ecb2cdea364f882aa8acd9d41
Ventsislav <ventsislav.dimitrov@softwaregroup.com>
2017-08-31 18:49:00 +0300

    fix: test ui/index

---
+++ ui/__tests__/Index-test.js
@@ -0,0 +1,9 @@
+/* global it, describe, expect */
+import index from '../index'
+
+describe('A suite for main index', function () {
+  it('should contain elements', function () {
+    expect(index.start).toBeInstanceOf(Function)
+    console.log(index.start.call({bus: {importMethod: () => () => {}}}))
+  })
+})
