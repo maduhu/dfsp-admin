a3630a13554a67b4a0e586b08bef786231d6be81
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-09-04 14:19:20 +0300

    fix: add test for reject batch

--- ui/ut-bulk/ui/react/containers/Batch/Popups/RejectBatch/__tests__/RejectBatch-test.js
+++ ui/ut-bulk/ui/react/containers/Batch/Popups/RejectBatch/__tests__/RejectBatch-test.js
@@ -1,9 +1,9 @@
 /* global it, describe, expect,beforeEach */
 import React from 'react'
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import configureStore from 'redux-mock-store'
-import {Map, List} from 'immutable'
-import {Provider} from 'react-redux'
+import { Map, List } from 'immutable'
+import { Provider } from 'react-redux'
 
 import RejectBatchPopup from '../index'
 
@@ -32,7 +32,37 @@ describe('A suite for <RejectBatchPopup /> popup', function () {
   })
 
   it('should contain elements', function () {
-    const wrapper = mount(<Provider store={store}><RejectBatchPopup /></Provider>, {context, childContextTypes})
+    const wrapper = mount(<Provider store={store}><RejectBatchPopup /></Provider>, { context, childContextTypes })
+    expect(wrapper.length).toBe(1)
+  })
+
+
+  it('should contain elements', function () {
+    const wrapper = mount(<Provider store={store}><RejectBatchPopup /></Provider>, { context, childContextTypes })
+    const wrapperComponent = shallow(React.createElement(
+      RejectBatchPopup.WrappedComponent,
+      {
+        actions: {
+          addComment: () => { },
+          rejectBatch: () => {
+            return Promise.resolve()
+          },
+          loadBatchStatuses: () => List([
+            { batchStatusId: 1, name: 'pending' },
+            { batchStatusId: 2, name: 'rejected' }
+          ]),
+          closeRejectBatchPopup: () => { }
+        },
+        batchStatuses: List([
+          { batchStatusId: 1, name: 'pending' },
+          { batchStatusId: 2, name: 'rejected' }
+        ])
+      })
+    )
+    // increase code coverage
+    wrapperComponent.instance().onClose({})
+    wrapperComponent.instance().handleAddComment({})
+    wrapperComponent.instance().onSubmit({})
     expect(wrapper.length).toBe(1)
   })
 })
